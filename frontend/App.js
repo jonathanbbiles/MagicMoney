18791efd5c2f6f2dd0ffb492fbd08a32181c06d1
jonathanbbiles <jonathanbbiles@gmail.com>
2026-01-15 22:05:18 -0800

    Fix DashboardScreen import path

--- frontend/App.js
+++ frontend/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import DashboardScreen from 'src/screens/DashboardScreen';
+import DashboardScreen from './src/screens/DashboardScreen';
 
 export default function App() {
   return <DashboardScreen />;
